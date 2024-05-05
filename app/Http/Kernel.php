fce5ac959d6d4f8306bd68639339e03d54344756
1Mein <119441676+1Mein@users.noreply.github.com>
2024-05-05 16:52:18 +0500

    start

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,9 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\OwnCors::class,
+//        \App\Http\Middleware\OwnCors::class,
+//        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+//        \Fruitcake\Cors\HandleCors::class, // Добавьте эту строку
     ];
 
     /**
